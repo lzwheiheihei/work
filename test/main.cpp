91e5ff3a7607e43b59c2674efc0161bc39b57870
Lzwheiheihei <794897980@qq.com>
2020-11-29 11:19:36 +0800

    test

---
+++ test/main.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<omp.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <iostream>
+#include <thread>
+using namespace std;
+int test(int num)
+{
+	cout << num << endl;
+	sleep(1);
+}
+int main()
+{
+	/*
+	#pragma omp parallel for num_threads(5)
+		for (int i = 0; i < 10; i++)
+		{
+			test(i);
+		}
+	*/
+	thread t[3];
+	for(int i = 0; i < 3; i++)
+	{
+		t[i] = thread(test, i);
+	}
+	for(int i = 0; i < 3; i++)
+	{
+		t[i].join();
+	}
+	return 0;
+}
